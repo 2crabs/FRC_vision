5aad2b877ec181aaad39e211666d9e05823e0182
Mickey <mickey_fraser@outlook.com>
2023-04-06 11:26:28 -0500

    Added template for cube and cone

--- vision.py
+++ vision.py
@@ -44,18 +44,46 @@ def on_event(event):
 i2c = I2C(I2C.I2C0, mode=I2C.MODE_SLAVE, scl=7, sda=8, addr = 1, addr_size=7, on_receive=on_receive, on_transmit=on_transmit, on_event=on_event)
 
 while True:
-    print(mode)
+    img=sensor.snapshot()
     if mode == 2:
         print("all good")
-        img=sensor.snapshot()
         img.cartoon()
         tags = img.find_apriltags(roi = (0,0,320,160), families = image.TAG16H5)# defaults to TAG36H11 without “families”
+        if tags.length() == 0:
+            active = 0
+        else:
+            active = 1
+
         for tag in tags:
             img.draw_rectangle(tag.rect(), color = (255, 0, 0))
             img.draw_cross(tag.cx(), tag.cy(), color = (0, 255, 0))
             degress = 180 * tag.rotation() / math.pi
             print(tag.id(),tag.z_translation())
             tag_id = tag.id()
+            tag_x = tag.cx()
+            tag_y = tag.cy()
+            tag.dist = tag.z_translation()
+    #cube
+    if mode == 1:
+        #need to add threshholds
+        blobs = img.find_blobs([cube_threshold], merge = True, pixels_threshold = 150)
+        if blobs:
+            for b in blobs:
+                tmp=img.draw_rectangle(b[0:4])
+                tmp=img.draw_cross(b[5], b[6])
+                c=img.get_pixel(b[5], b[6])
+        lcd.display(img)
+
+    #cone
+    if mode == 0:
+        #need to add threshholds
+        blobs = img.find_blobs([cone_threshold], merge = True, pixels_threshold = 150)
+        if blobs:
+            for b in blobs:
+                tmp=img.draw_rectangle(b[0:4])
+                tmp=img.draw_cross(b[5], b[6])
+                c=img.get_pixel(b[5], b[6])
+        lcd.display(img)
 
 
 
